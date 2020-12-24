c0fc79ea406470eaab75108b083e0e1917ae6b25
ShubhamKotal <56103529+shubhamsoulpage@users.noreply.github.com>
2020-12-24 15:33:23 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,23 @@
+
+
+import numpy as np
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return <h1> Hello </h1>
+
+    # return render_template('Home.html')
+
+
+if __name__ == "__main__":	
+    app.run(debug=True)
+
+
+
+
+
+
